a971e0527a09bf15a237d4018106818aaf60892a
Diego Maroto <diego.diemg@gmail.com>
2024-11-19 16:07:56 +0100

    Fixing code to use repo and owner correctly

--- src/action.js
+++ src/action.js
@@ -4,21 +4,30 @@ const github = require("@actions/github");
 async function run() {
   try {
     const token = core.getInput("token");
+    const repository = core.getInput("repository");
     const eventType = core.getInput("event-type");
     const clientPayload = JSON.parse(core.getInput("client-payload"));
 
+    const [owner, repo] = repository.split("/");
     const octokit = github.getOctokit(token);
 
     await octokit.rest.repos.createDispatchEvent({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
+      owner: owner,
+      repo: repo,
       event_type: eventType,
-      client_payload: clientPayload,
+      client_payload: JSON.parse(clientPayload),
     });
 
     core.info(`Successfully triggered ${eventType} event`);
   } catch (error) {
-    core.setFailed(`Action failed with error: ${error}`);
+    core.debug(inspect(error));
+    if (hasErrorStatus(error) && error.status == 404) {
+      core.setFailed(
+        "Repository not found, OR token has insufficient permissions."
+      );
+    } else {
+      core.setFailed(getErrorMessage(error));
+    }
   }
 }
 
