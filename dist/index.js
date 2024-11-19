4ce915516e27c00b3097d691b786128e356a2b96
Diego Maroto <diego.diemg@gmail.com>
2024-11-19 16:08:10 +0100

    Build

--- dist/index.js
+++ dist/index.js
@@ -23851,18 +23851,27 @@ var require_action = __commonJS({
     async function run2() {
       try {
         const token = core.getInput("token");
+        const repository = core.getInput("repository");
         const eventType = core.getInput("event-type");
         const clientPayload = JSON.parse(core.getInput("client-payload"));
+        const [owner, repo] = repository.split("/");
         const octokit = github.getOctokit(token);
         await octokit.rest.repos.createDispatchEvent({
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo,
+          owner,
+          repo,
           event_type: eventType,
-          client_payload: clientPayload
+          client_payload: JSON.parse(clientPayload)
         });
         core.info(`Successfully triggered ${eventType} event`);
       } catch (error) {
-        core.setFailed(`Action failed with error: ${error}`);
+        core.debug(inspect(error));
+        if (hasErrorStatus(error) && error.status == 404) {
+          core.setFailed(
+            "Repository not found, OR token has insufficient permissions."
+          );
+        } else {
+          core.setFailed(getErrorMessage(error));
+        }
       }
     }
     module2.exports = { run: run2 };
